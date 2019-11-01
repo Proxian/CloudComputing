bba36c587b7c7b5298fbb2d8bb5b7a66c5879cb4
Proxian <37815980+Proxian@users.noreply.github.com>
2019-11-01 14:31:45 -0500

    Create Lab4f.py

---
+++ Lab4/Lab4f.py
@@ -0,0 +1,11 @@
+"""
+Python Boto3 Program Lab4b
+"""
+import sys
+import boto3
+from botocore.exceptions import ClientError
+
+ec2 =  boto3.client('ec2')
+
+response = ec2.create_key_pair(KeyName='ryanLab41fkey')
+print('Success',response)
